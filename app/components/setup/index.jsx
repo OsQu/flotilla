f2f52468f532666aa284b9018eb27667d29817b1
Oskari Virtanen <oskari@smartly.io>
2017-01-27 11:57:14 +0200

    Add flow

--- app/components/setup/index.jsx
+++ app/components/setup/index.jsx
@@ -1,7 +1,11 @@
+/* @flow */
+
 import React from 'react';
 
 export default class Setup extends React.Component {
   render() {
-    return <h1>We are doing this!</h1>;
+    const foo: string = "Oskari";
+
+    return <h1>We are doing this {foo}!</h1>;
   }
 }
