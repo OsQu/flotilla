213567ab54ab1c04c51359105586138e04c07b6f
Oskari Virtanen <oskari@flowdock.com>
2017-01-28 14:59:20 +0200

    Use photon for UI

--- app/components/setup/Setup.jsx
+++ app/components/setup/Setup.jsx
@@ -5,6 +5,8 @@ import React from 'react';
 export default class Setup extends React.Component {
   render() {
     const foo: string = "Oskari";
-    return <h1>We are doing this {foo}!</h1>;
+    return <div className="padded-more">
+      <span>We are doing this {foo}!</span>
+    </div>;
   }
 }
--- app/main.jsx
+++ app/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory} from 'react-router';
+import { Router, Route, IndexRoute, hashHistory} from 'react-router';
 
+import App from './app';
 import Setup from './components/setup';
 
 window.onload = function() {
   render(
     <Router history={hashHistory}>
-      <Route path="/" component={Setup} />
+      <Route path="/" component={App}>
+        <IndexRoute component={Setup} />
+      </Route>
     </Router>, document.getElementById('app'));
 };
